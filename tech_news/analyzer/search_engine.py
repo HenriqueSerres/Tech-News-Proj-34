3bf67a9437f083643289e8abf06c9a1af32ec4e2
HenriqueSerres <henriqueserres@gmail.com>
2022-09-29 12:19:48 -0300

    req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
+# Onde pesquisei o regex da query no mongoDB
+# https://www.mongodb.com/docs/manual/reference/operator/query/regex/
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    the_news = search_news({'title': {'$regex': title}, '$options': 'i'})
+    news_list = [(news['title'], news['url']) for news in the_news]
+    return news_list
 
 
 # Requisito 7
