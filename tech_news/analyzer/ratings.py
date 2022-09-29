c61687754381228058612ad82a39c0e3581e758d
HenriqueSerres <henriqueserres@gmail.com>
2022-09-29 16:13:19 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,21 @@
+from operator import itemgetter
+from tech_news.database import search_news
+
+# Onde pesquisei função sorted:
+# https://www.w3schools.com/python/ref_func_sorted.asp
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    all_news = search_news({})
+    top_five = []
+    sorted_news = sorted(
+        all_news, key=itemgetter("comments_count"), reverse=True
+    )
+    for news in sorted_news[:5]:
+        top_five.append((news["title"], news["url"]))
+    print(top_five)
+    return top_five
 
 
 # Requisito 11
